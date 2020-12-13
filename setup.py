dfaaaf4464fa07169b12f373cd3a80534daefc89
Jan Krepl <jankrepl@yahoo.com>
2020-12-13 21:48:31 +0100

    Add boilerplate

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+import gnnfit
+
+DESCRIPTION = ""
+LONG_DESCRIPTION = DESCRIPTION
+
+INSTALL_REQUIRES = [
+    "torch",
+]
+
+setup(
+    name="gnnfit",
+    version=gnnfit.__version__,
+    author="Jan Krepl",
+    author_email="kjan.official@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/jankrepl/gnnfit",
+    packages=find_packages(exclude=["tests"]),
+    license="MIT",
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        "dev": [
+            "black",
+            "flake8",
+            "pydocstyle",
+            "pytest",
+            "pytest-coverage",
+            "tox",
+        ],
+    },
+)
